1be9eb06bc413441e8828bd0bf2d82058cc3e3d7
Matheus Luiz <142438014+MatheusLMMARCILIO@users.noreply.github.com>
2024-06-21 17:07:07 -0300

    21/06/2024 17h07

--- src/App.js
+++ src/App.js
@@ -1,64 +1,31 @@
-import React from 'react';
-import './App.css';
+import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from '/PAGES/inicio/index.js'
+
+import './App.css'
+
+import Home from './PAGES/inicio/index.js'
+
+const PageHome = () => {
+
+    return (<>
+
+        <section className="Menu">
+            <div className="LogoMenu" >
+                <img src="./IMAGE/NetflixLogo.webp" alt="logo" />
+            </div>
+
+            <div className="linkMenu" >
+<Router>
+    <Route path="/" element={<Home />}></Route>
+</Router>
+
+            </div>
 
 
-
-const Login = () => {
-
-const Name = 'Matheus'
-
-
-const InputName = document.querySelector('.InputName')
-const Password = document.querySelector('.InputPassword')
-
-function NextPage() {
-    if (InputName.value == Name && Password.value == '123' ) {
-window.location.href = './PAGES/WhoWatching/index.js'
-    }
-}
-
-
-
-
-
-    return (
-        <section className='AllPage'>
-            <section className='Logo' >
-                <img src='./IMAGE/NetflixLogo.webp' alt='Logo' />
-            </section>
-            <section className='Login' >
-                <section className='LoginInputs' >
-
-                    <div className='Enter' >
-                        <span id='EnterPageNetflix' >Entrar</span>
-                    </div>
-
-                    <div className='InputLogin' >
-
-                        <input type='text' className='InputName' placeholder='Email ou número de celular' />
-                        <input type='password' className='InputPassword' placeholder='Senha' />
-
-                        <button onClick={NextPage} >Entrar</button>
-
-                    </div>
-
-                    <div className='Or' >
-                        <span>OU</span>
-                    </div>
-
-
-                    <div className='UseCodig' >
-                        <button  ><span>Usar um código de acesso</span></button>
-                    </div>
-
-                    <div className='ForgetPassword' >
-                        <span> esqueceu a senha? </span>
-                    </div>
-
-                </section>
-            </section>
         </section>
-    )
-}
 
-export default Login
+
+    </>)
+}
+export default PageHome
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Login from './App';
+import PageMenu from './App.js'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+<PageMenu />
   </React.StrictMode>
 );
